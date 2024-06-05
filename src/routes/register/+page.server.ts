bff98ecc928a4d5f9595cf98c45c5b98c74d5249
Takuya Iwamoto <iwamoto.takuya@pintinc.jp>
2024-06-05 23:34:53 +0900

    20240605

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,19 +1,4 @@
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
-import { expoOut } from "svelte/easing";
-// import bcrypt from 'bcrypt'
-// import { db } from "$lib/prisma";
-
-export const action : Actions = {
-    register : async ({request}) => {
-        const data = await request.formData();
-        const name = data.get("name");
-        const password = data.get("password");
-
-        if (typeof name !== "string" || typeof password ! == "string" || !name || !password) {
-            return fail(400, {message: "名前・パスワードは必須です・
-            
-            "})
-        }
-    }
-}
+import bcrypt from 'bcrypt';
+import { db } from "$lib/prisma";
